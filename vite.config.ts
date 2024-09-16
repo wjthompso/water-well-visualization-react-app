89c286a2b1e5c23785314fe9651b4077dbda51ef
wjthompso <49210425+wjthompso@users.noreply.github.com>
2024-09-16 12:19:11 -0700

    Fixing image loading issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,35 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import cesium from "vite-plugin-cesium";
+// import cesium from "vite-plugin-cesium";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import svgr from "vite-plugin-svgr";
+
+const cesiumSource = "node_modules/cesium/Build/Cesium";
+// This is the base url for static files that CesiumJS needs to load.
+// Set to an empty string to place the files at the site's root path
+const cesiumBaseUrl = "cesiumStatic";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    define: {
+        // Define relative base path in cesium for loading assets
+        // https://vitejs.dev/config/shared-options.html#define
+        CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
+    },
     base: "/",
-    plugins: [react(), cesium(), svgr()],
+    plugins: [
+        react(),
+        // cesium(),
+        svgr(),
+        viteStaticCopy({
+            targets: [
+                { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
+                { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
+                { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
+                { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
+            ],
+        }),
+    ],
     server: {
         fs: {
             // Allow Vite's server to access files outside of the root directory
