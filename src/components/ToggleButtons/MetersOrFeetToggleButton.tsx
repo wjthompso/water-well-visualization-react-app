6d177079badbbf1e597822015968f800f979a176
wjthompso <49210425+wjthompso@users.noreply.github.com>
2024-10-23 21:23:57 -0700

    Updating MetersOrFeetToggleButton.tsx

--- src/components/ToggleButtons/MetersOrFeetToggleButton.tsx
+++ src/components/ToggleButtons/MetersOrFeetToggleButton.tsx
@@ -17,8 +17,8 @@ const MetersOrFeetToggleButton: React.FC<MetersOrFeetToggleButtonProps> = ({
             <button
                 className={`flex-1 flex items-center justify-center ${
                     metersOrFeet === "feet"
-                        ? "bg-white"
-                        : "bg-[#A9A9A9] shadow-[inset_0_4px_4px_0_rgba(0,0,0,0.25)]"
+                        ? "bg-[#A9A9A9] shadow-[inset_0_4px_4px_0_rgba(0,0,0,0.25)]"
+                        : "bg-white"
                 } rounded-l-[2.2px] h-[18px] transition-colors duration-300 ease-in-out font-bold text-[12px]`}
                 onClick={() => setMetersOrFeet("feet")}
             >
@@ -27,8 +27,8 @@ const MetersOrFeetToggleButton: React.FC<MetersOrFeetToggleButtonProps> = ({
             <button
                 className={`flex-1 flex items-center justify-center ${
                     metersOrFeet === "meters"
-                        ? "bg-white"
-                        : "bg-[#A9A9A9] shadow-[inset_0_4px_4px_0_rgba(0,0,0,0.25)]"
+                        ? "bg-[#A9A9A9] shadow-[inset_0_4px_4px_0_rgba(0,0,0,0.25)]"
+                        : "bg-white"
                 } rounded-r-[2.2px] h-[18px] transition-colors duration-300 ease-in-out font-bold text-[12px]`}
                 onClick={() => setMetersOrFeet("meters")}
             >
