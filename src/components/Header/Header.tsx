a4dadbcd1536ebf924a8d2a93cb9c4a2e37e19de
wjthompso <49210425+wjthompso@users.noreply.github.com>
2024-11-06 13:33:37 -0800

    Adding NSF Logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,23 @@
 import { FC } from "react";
+import NSFLogo from "../../assets/NSFLogo.png";
 import UCSBBrenLogo from "../../assets/UCSBBrenLogo.svg";
 
 const Header: FC = () => {
     return (
         <header className="py-4 h-[3.5rem] w-full md:h-[89px] flex items-center bg-headerBackgroundColor border-b-[0.5px] border-[#808080]">
             <div className="flex items-center w-full px-4">
-                <img
-                    id="UCSBBrenLogo"
-                    src={UCSBBrenLogo}
-                    alt="UCSB Bren Logo"
-                    className="flex-shrink-0 mr-4 w-[10rem] h-[4rem] md:w-[15rem] md:h-[3.5rem]"
-                />
+                <a
+                    href="https://bren.ucsb.edu/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        id="UCSBBrenLogo"
+                        src={UCSBBrenLogo}
+                        alt="UCSB Bren Logo"
+                        className="flex-shrink-0 mr-4 w-[10rem] h-[4rem] md:w-[15rem] md:h-[3.5rem]"
+                    />
+                </a>
                 <div className="flex-grow"></div>
                 <div
                     id="header-text"
@@ -29,6 +36,12 @@ const Header: FC = () => {
                     id="element-the-same-width-as-the-logo"
                     className="mr-4 flex-shrink-0 max-w-[15rem] h-[3.5rem]"
                 ></div>
+                <img
+                    id="NSFLogo"
+                    src={NSFLogo}
+                    alt="NSF Logo"
+                    className="flex-shrink-0 w-[3rem] h-[3rem] md:w-[3.5rem] md:h-[3.5rem]"
+                />
             </div>
         </header>
     );
